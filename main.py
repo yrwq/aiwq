991949cbcc6acd44b3b887b18c51f1d7ca5da3e3
Inhof David <yrwqid@gmail.com>
2024-01-28 01:21:04 +0100

    basic setup of ollama

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ import asyncio
 import psutil    
 import stat
 import os
+from subprocess import Popen, PIPE, STDOUT
+try:
+    from subprocess import DEVNULL # py3k
+except ImportError:
+    import os
+    DEVNULL = open(os.devnull, 'wb')
 
 ollama_exec_path = "./ollama"
 ollama_version = "v0.1.22"
+ollama_model = "orca-mini"
 
 urls = {
     "darwin": f"https://github.com/ollama/ollama/releases/download/{ollama_version}/ollama-darwin",
@@ -30,6 +37,8 @@ async def ollama_download():
     sys_plat = platform.system()
     sys_arch = platform.machine()
 
+    print(f"downloading ollama version {ollama_version}")
+
     match sys_plat:
         case "Darwin":
             urlretrieve(urls["darwin"], ollama_exec_path)
@@ -47,34 +56,34 @@ async def ollama_download():
             exit(1)
             
 
-    print(f"downloading ollama version {ollama_version}")
     print("ollama downloaded successfully")
 
 async def ollama_serve():
-    from subprocess import Popen, PIPE, STDOUT
-
-    try:
-        from subprocess import DEVNULL # py3k
-    except ImportError:
-        import os
-        DEVNULL = open(os.devnull, 'wb')
-
     p = Popen([ollama_exec_path, "serve"], stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
-
     # sleep 1 just in case before pulling/running
     await asyncio.sleep(1)
     
     return p
 
-async def ollama_check_models():
+async def ollama_check_models() -> bool :
     """
     Check if any model is already pulled
+    Returns true if a model is already installed
     """
     result = subprocess.run([ollama_exec_path, "list"], stdout=subprocess.PIPE)
     pretty_result = result.stdout.decode("utf-8")
 
     lines = pretty_result.splitlines()
-    return len(lines)
+    if len(lines) != 1 : return True
+    else: return False
+
+async def ollama_pull_model(model):
+    result = subprocess.run([ollama_exec_path, "pull", model], stdout=subprocess.PIPE)
+    pretty_result = result.stdout.decode("utf-8")
+    print(pretty_result)
+
+async def ollama_run_model(model):
+    p = Popen([ollama_exec_path, "serve"], stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
 
 async def main():
     # check if ollama executable is available
@@ -95,9 +104,11 @@ async def main():
     if "ollama" not in (p.name() for p in psutil.process_iter()):
         await ollama_serve()
 
-    models = await ollama_get_models()
-    print(models)
-    # await pull
-    # await run
+    model_installed = await ollama_check_models()
+
+    if model_installed == False:
+        await ollama_pull_model(ollama_model)
+
+    await ollama_run_model(ollama_model)
 
 asyncio.run(main())
